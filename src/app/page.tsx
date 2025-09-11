d9dc003f65331218b5491fc957d39787cdf73296
joseperezdmgz <joseperezdmgz@gmail.com>
2025-09-11 14:37:18 +0200

    fix: error de despliegue porque no tenia suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import PokemonGrid from "@/components/PokemonGrid";
 
 export default function HomePage() {
@@ -6,7 +7,10 @@ export default function HomePage() {
       <div className="mb-6 flex flex-col sm:flex-row gap-4">
         <p className="text-white">Filtros</p>
       </div>
-      <PokemonGrid />
+
+      <Suspense fallback={<p className="text-white">Cargando pokemons...</p>}>
+        <PokemonGrid />
+      </Suspense>
     </div>
   );
 }
